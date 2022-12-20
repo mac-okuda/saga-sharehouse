1609fdecb445329b5802f197329deaf1034b5afa
okuden127 <o_10476@yahoo.co.jp>
2022-12-20 10:50:35 +0900

    Edit:日付とtagの設定

--- .eleventy.js
+++ .eleventy.js
@@ -13,8 +13,36 @@ module.exports = function(eleventyConfig) {
     return collection.getFilteredByGlob("src/works/*.md");
   });
 
-    function filterTagList(tags) {
-    return (tags || []).filter(tag => ["all", "blog", "post", "posts"].indexOf(tag) === -1);
+
+  
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy/MM/dd');
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-MM-dd');
+  });
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if(!Array.isArray(array) || array.length === 0) {
+      return [];
+    }
+    if( n < 0 ) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
+  // Return the smallest number argument
+  eleventyConfig.addFilter("min", (...numbers) => {
+    return Math.min.apply(null, numbers);
+  });
+
+  function filterTagList(tags) {
+    return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
   }
 
   eleventyConfig.addFilter("filterTagList", filterTagList)
@@ -29,14 +57,6 @@ module.exports = function(eleventyConfig) {
     return filterTagList([...tagSet]);
   });
   
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
-  });
-
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-  });
   return {
     dir: {
       input: "src",
